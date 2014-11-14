dfdd01d430e3aaf9f41171401082d572c31dd985
Jean Ro <jsro88@gmail.com>
2014-11-13 21:54:29 -0800

    log excercise

---
+++ Sandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sandbox
+//
+//  Created by Jean Ro on 11/13/14.
+//  Copyright (c) 2014 Jean Ro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sandbox/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sandbox
+//
+//  Created by Jean Ro on 11/13/14.
+//  Copyright (c) 2014 Jean Ro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
